9e0767441b71dfdbd0574cfa9f648e5cbeb1c753
Cloudtastrophe <ifiwil@yahoo.com>
2023-11-15 16:21:54 -0800

    Another test commit

--- _printf.c
+++ _printf.c
@@ -1,58 +1,64 @@
 #include "main.h"
 
 /**
- * _printf - Prints formatted output.
+ * print_buffer - Prints the contents of the buffer if it exists.
+ * @buffer: Array of chars.
+ * @buff_ind: Index at which to add the next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
+
+/**
+ * _printf - Printf function.
  * @format: Format string.
  *
- * Return: The length of the string.
+ * Return: Number of printed characters.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-    convert p[] = {
-        {"%s", print_s}, {"%c", print_c},
-        {"%%", print_37},
-        {"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
-        {"%R", print_rot13}, {"%b", print_bin},
-        {"%u", print_unsigned},
-        {"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
-        {"%S", print_exc_string}, {"%p", print_pointer}
-    };
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_list args;
-    int i = 0, j, length = 0;
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
+	va_start(list, format);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            for (j = 13; j >= 0; j--)
-            {
-                if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
-                {
-                    length += p[j].function(args);
-                    break;
-                }
-            }
-            if (j < 0)  /* No matching specifier found, print character */
-            {
-                _putchar(format[i - 1]);
-                length++;
-            }
-            i++;  /* Skip the specifier character */
-        }
-        else
-        {
-            _putchar(format[i]);
-            length++;
-        }
-        i++;
-    }
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-    va_end(args);
-    return (length);
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
